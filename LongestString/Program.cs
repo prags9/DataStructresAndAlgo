85256ef0a4a284e8a29c90ae0798543f787f83f3
Pragati Pathak <pragati.pthk@gmail.com>
2020-06-26 18:58:26 +0530

    Arrays part2

---
+++ LongestString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LongestString
+{
+    class Program
+    {
+        public string LongestWord(string sen)
+        {
+            int len = 0;
+            string[] strArray = sen.Split();
+            var regexItem = new Regex("^[A-Z]*[a-z]*$");
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (regexItem.IsMatch(strArray[i]))
+                {
+                    if (strArray[i].Length > len)
+                    {
+                        len = strArray[i].Length;
+                        sen = strArray[i];
+                    }
+                }                 
+
+            }
+
+            return sen;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Console.WriteLine(p.LongestWord("Argument goes here"));
+        }
+    }
+}
--- RotateArray/Program.cs
+++ RotateArray/Program.cs
@@ -4,6 +4,8 @@ namespace RotateArray
 {
     class Program
     {
+        //Time Complexity = O(n)
+        //Space Complexity = O(n)
         public void Rotate(int[] nums, int k)
         {
             int[] rotateArray = new int[nums.Length];
@@ -32,22 +34,65 @@ namespace RotateArray
                 Console.WriteLine(rotateArray[i]);
             }
         }
-
+        //Time Complexity = O(n)
+        //Space Complexity = O(n)
         public void Rotate1(int[] nums, int k)
         {
-            int[] rotateArray = new int[nums.Length];
-
-
-
-            for (int i = 0; i < rotateArray.Length; i++)
+            int[] res = new int[nums.Length];
+            int len = nums.Length;
+            k = k % len;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int index = (i - k) >= 0 ? (i - k) : (len + i - k);
+                    res[i] = nums[index];
+                }
+            
+            nums = res;
+            for (int i = 0; i < nums.Length; i++)
             {
-                Console.WriteLine(rotateArray[i]);
+                Console.WriteLine(nums[i]);
             }
+        }
+
+        //Time Complexity = O(n)
+        //Space Complexity = O(1)
+        public void Rotate2(int[] nums, int k)
+        {
+            int len = nums.Length;
+            k = k % len;
+            int gcd = Gcd(k, len);
+            for (int i = len-gcd; i < len; i++)
+            {
+                int x = i,y=1;
+                int temp = nums[i];
+                while (y>=0)
+                {
+                    y = x-gcd;
+                    if(y>=0)
+                    nums[x] = nums[y];
+                    x = y;
+                }
+                nums[x+gcd] = temp;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        private int Gcd(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            else
+                return Gcd(b, a % b);
         }
         public static void Main(string[] args)
         {
             Program p = new Program();
-            p.Rotate(new int[] {-1,-100,3,99}, 2);  
+            p.Rotate1(new int[] { -1, -100, 3, 99 }, 2);  
+            //p.Rotate1(new int[] { 1,2,3,4,5,6,7}, 3);
         }
     }
 }
